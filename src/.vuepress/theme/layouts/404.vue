8c0b928f105e06d5a9ba25f549346fc244da6189
buyaobishige <1105936025@163.com>
2019-09-22 20:03:45 +0800

    init

---
+++ src/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>404</h1>
+    <p><strong>File not found</strong></p>
+    <p>
+      The site configured at this address does not
+      contain the requested file.
+    </p>
+    <a href="http://wpa.qq.com/msgrd?v=3&uin=838801702&site=qq&menu=yes">联系我们</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  margin: 50px auto 40px auto;
+  width: 600px;
+  text-align: center;
+}
+</style>
