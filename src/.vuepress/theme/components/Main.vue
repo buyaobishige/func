5ed7e61db5bca7e3b698d9fc4b8b21092c4cd661
buyaobishige <1105936025@163.com>
2019-09-29 19:08:45 +0800

    1

---
+++ src/.vuepress/theme/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{a}}</h1>
+    <h1>hello</h1>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  data () {
+    return { a: 'hello' };
+  }
+};
+</script>
